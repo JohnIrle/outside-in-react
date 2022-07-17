9550cf29a3c7695412ec50a222f0e832121de077
John Irle <irlejohn@gmail.com>
2022-07-17 15:51:35 -0500

    feat: connect component to redux and export unconnected function for testing.

--- src/components/RestaurantList.spec.js
+++ src/components/RestaurantList.spec.js
@@ -1,4 +1,4 @@
-import RestaurantList from './RestaurantList';
+import {RestaurantList} from './RestaurantList';
 import {render, screen} from '@testing-library/react';
 
 describe('RestaurantList', () => {
